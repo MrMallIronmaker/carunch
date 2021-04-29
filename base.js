01d2b2d1c2002d8dbb22955a626f4722f73f1dd1
MrMallIronmaker <725mrm@gmail.com>
2021-04-29 13:24:47 -0700

    update pre mtg

--- base.js
+++ base.js
@@ -1,33 +1,52 @@
 // Our input frames will come from here.
 const videoElement = document.getElementsByClassName('input_video')[0];
 
-let scene, camera, renderer, moveCube;
+let scene, camera, renderer;
 
 // Create an empty scene
 
 const fov = 45;
 const videoScreenDistance = 900.0;
+const screenWidth = 1280;
+const screenHeight = 720;
+const aspect = screenWidth/screenHeight;
+const cylinderPos = new THREE.Vector3(-0.12, -0.08, -0.3);
 
+function screenSpaceToWorldSpace(px, py, distance) {
+    const factor = distance * Math.tan(Math.PI * fov / 2 / 180);
+    return new THREE.Vector3(
+        (1 - 2 * px) * aspect * factor,
+        (1 - 2 * py) * factor,
+        -distance,
+    )
+}
 
 const cube = new THREE.Mesh(
     new THREE.BoxGeometry( 0.02, 0.02, 0.02),
-    new THREE.MeshBasicMaterial( { color: "#433F81" } )
+    new THREE.MeshStandardMaterial( { color: "#433F81", roughness: 0.4} )
 );
 
+function buildLights(scene) {
+    var light = new THREE.SpotLight("#fff", 0.8);
+    light.position.y = 100;
 
-function initRenderer() {
-    const aspect = videoElement.videoWidth/videoElement.videoHeight;
+    light.angle = 1.05;
 
-    scene = new THREE.Scene();
-    camera = new THREE.PerspectiveCamera( fov, aspect, 0.1, 1000 );
-    renderer = new THREE.WebGLRenderer({antialias:true});
-    renderer.setClearColor("#000000");
-    renderer.setSize( videoElement.videoWidth, videoElement.videoHeight );
-    document.getElementById("rendererTarget").append(renderer.domElement);
+    light.decacy = 2;
+    light.penumbra = 1;
+
+    light.shadow.camera.near = 10;
+    light.shadow.camera.far = 1000;
+    light.shadow.camera.fov = 30;
+
+    scene.add(light);
+    return light;
+}
+
+function buildVideoTexture(scene) {
     const videoTexture = new THREE.VideoTexture(videoElement);
     videoTexture.minFilter = THREE.LinearFilter;
     const videoMaterial =  new THREE.MeshBasicMaterial( {map: videoTexture, side: THREE.BackSide, toneMapped: false} );
-    //Create screen
     const screen = new THREE.PlaneGeometry(
         2 * aspect * videoScreenDistance * Math.tan(Math.PI * fov / 2 / 180),
         2 * videoScreenDistance * Math.tan(Math.PI * fov / 2 / 180),
@@ -37,16 +56,34 @@ function initRenderer() {
     videoScreen.rotation.y = Math.PI;
     scene.add(videoScreen);
 
-    moveCube = function(px, py) {
-        const distance = 0.3;
-        const factor = distance * Math.tan(Math.PI * fov / 2 / 180);
-        videoElement.videoHeight
-        cube.position.x = (1 - 2 * px) * aspect * factor;
-        cube.position.y = (1 - 2 * py) * factor;
-        cube.position.z = -distance;
-    }
+}
 
-    cube.position.z = -4;
+function buildChipBag(scene) {
+    const geometry = new THREE.CylinderGeometry( 0.025, 0.025, 0.05, 16);
+    const material = new THREE.MeshStandardMaterial( {color: 0xffff00, roughness: 0.2} );
+    const cylinder = new THREE.Mesh( geometry, material );
+    cylinder.position.copy(cylinderPos);
+    cylinder.rotation.x = Math.PI / 3;
+    cylinder.rotation.y = Math.PI / 8;
+    cylinder.rotation.z = -Math.PI / 2;
+    scene.add( cylinder );
+}
+
+
+function initRenderer() {
+
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( fov, aspect, 0.1, 1000 );
+    renderer = new THREE.WebGLRenderer({antialias:true});
+    renderer.setClearColor("#000000");
+    renderer.setSize( videoElement.videoWidth, videoElement.videoHeight );
+    document.getElementById("rendererTarget").append(renderer.domElement);
+
+    buildChipBag(scene);
+    buildVideoTexture(scene);
+    buildLights(scene);
+
+    cube.position.z = -0.3;
     scene.add(cube);
 
     const render = function () {
@@ -63,37 +100,6 @@ videoElement.addEventListener('loadedmetadata', (event) => {
     initRenderer();
 })
 
-
-
-// ------------------------------------------------
-// FUN STARTS HERE
-// ------------------------------------------------
-
-// Create a Cube Mesh with basic material
-// var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-
-// const texture = new THREE.VideoTexture( videoElement );
-
-// const geometry = new THREE.PlaneBufferGeometry();
-// const material = new THREE.MeshBasicMaterial( { map: texture } );
-
-// var material = new THREE.MeshBasicMaterial( { color: "#433F81" } );
-
-// let cube = new THREE.Mesh( geometry, material );
-
-// Add cube to Scene
-// scene.add( cube );
-
-
-
-// Render Loop
-
-
-
-// We'll add this to our control panel later, but we'll save it here so we can
-// call tick() each time the graph runs.
-const fpsControl = new FPS();
-
 // Optimization: Turn off animated spinner after its hiding animation is done.
 const spinner = document.querySelector('.loading');
 spinner.ontransitionend = () => {
@@ -107,13 +113,6 @@ function onResults(results) {
     // Hide the spinner.
     document.body.classList.add('loaded');
 
-
-    // Update the frame rate.
-    // fpsControl.tick();
-
-    // Draw the overlays.
-    // renderer.drawImage(results.image, 0, 0, renderer.width, renderer.height);
-
     // get lip points
     if (results.multiFaceLandmarks) {
         for (const landmarks of results.multiFaceLandmarks) {
@@ -124,41 +123,32 @@ function onResults(results) {
                 Math.pow(landmarks[291].x - landmarks[62].x, 2) +
                 Math.pow(landmarks[291].y - landmarks[62].y, 2);
 
-            moveCube(
-                (landmarks[13].x + landmarks[14].x)/2,
-                (landmarks[13].y + landmarks[14].y)/2
-            );
-
             if (isMouthOpen) {
                 // canvasCtx.fillStyle = "#30ff30";
-                if (!wasPreviousMouthOpen) {
-                    // mouth just opened,
-                    // make the object fly to the mouth.
-                }
+                let target = screenSpaceToWorldSpace(
+                    (landmarks[13].x + landmarks[14].x)/2,
+                    (landmarks[13].y + landmarks[14].y)/2,
+                    0.3
+                );
+                const alpha = 0.3;
+                //console.log(target, cube.position);
+                cube.position.addVectors(cube.position.multiplyScalar(1 - alpha), target.multiplyScalar(alpha));
 
             } else {
                 //canvasCtx.fillStyle = "#ff3030";
                 if (isSoundPlayable && wasPreviousMouthOpen) {
                     isSoundPlayable = false;
                     new Audio("crunch_sound.ogg").play();
+                    cube.position.copy(cylinderPos);
                     setTimeout(
                         function() {isSoundPlayable = true;},
                         300
                     )
                 }
             }
-            //canvasCtx.rect(10, 20, 150, 100);
-            //canvasCtx.fill();
-
             wasPreviousMouthOpen = isMouthOpen;
-
-            // do some hysteresis
         }
     }
-
-    //canvasCtx.restore();
-    // renderer.render(scene, camera);
-
 }
 
 const faceMesh = new FaceMesh({locateFile: (file) => {
