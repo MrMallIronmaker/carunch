0b6d04f7da9e4abaf51f19dc880f6cef6bee9125
MrMallIronmaker <725mrm@gmail.com>
2021-05-05 15:14:27 -0700

    recording is button-enabled

--- base.js
+++ base.js
@@ -2,6 +2,8 @@
 const videoElement = document.getElementsByClassName('input_video')[0];
 
 let scene, camera, renderer, plate, edible, edibles, crunches;
+let mediaRecorder, recordedBlobs, sourceBuffer;
+let stream;
 
 // Create an empty scene
 
@@ -10,7 +12,7 @@ const videoScreenDistance = 900.0;
 const screenWidth = 1280;
 const screenHeight = 720;
 const aspect = screenWidth/screenHeight;
-const cylinderPos = new THREE.Vector3(-0.12, -0.08, -0.3);
+let isFoodAvailable = false;
 
 function screenSpaceToWorldSpace(px, py, distance) {
     const factor = distance * Math.tan(Math.PI * fov / 2 / 180);
@@ -30,6 +32,36 @@ function buildSpriteFromImgURL(url) {
         map: new THREE.TextureLoader().load(url)
     }));
 }
+
+const next_button = document.getElementById("next_button");
+const instructions_text = document.getElementById("instructions_text");
+const instructions_box = document.getElementById("instructions_box");
+
+function forwardButton() {
+    next_button.step = next_button.step + 1;
+    switch(next_button.step) {
+        case 1:
+            instructions_text.textContent = "First, ensure your sound is on.";
+            break;
+        case 2:
+            instructions_text.textContent = "Next, open and close your mouth." +
+                "If you have trouble, make sure your face is well-lit and within view of the camera. " +
+                "If you still have trouble, alert the course staff.";
+            next_button.disabled = true;
+            break;
+        case 3:
+            instructions_text.textContent = "Finally, be aware that the video will be recorded " +
+                "as you interact with the virtual content.";
+            break;
+        default:
+            instructions_box.hidden = true;
+            buildPlate(scene);
+
+    }
+}
+
+next_button.step = 0;
+next_button.onclick = forwardButton;
 
 // this one returns an array of makhanas
 function buildMakhanas() {
@@ -65,23 +97,7 @@ function buildPlate(scene) {
 
     edible = edibles[getRandomInt(edibles.length)].clone();
     scene.add(edible);
-}
-
-function buildLights(scene) {
-    var light = new THREE.SpotLight("#fff", 0.8);
-    light.position.y = 100;
-
-    light.angle = 1.05;
-
-    light.decacy = 2;
-    light.penumbra = 1;
-
-    light.shadow.camera.near = 10;
-    light.shadow.camera.far = 1000;
-    light.shadow.camera.fov = 30;
-
-    scene.add(light);
-    return light;
+    isFoodAvailable = true;
 }
 
 function buildVideoTexture(scene) {
@@ -99,7 +115,6 @@ function buildVideoTexture(scene) {
 
 }
 
-
 function initRenderer() {
 
     scene = new THREE.Scene();
@@ -109,10 +124,7 @@ function initRenderer() {
     renderer.setSize( videoElement.videoWidth, videoElement.videoHeight );
     document.getElementById("rendererTarget").append(renderer.domElement);
 
-    // buildChipBag(scene);
     buildVideoTexture(scene);
-    buildLights(scene);
-    buildPlate(scene);
 
     const render = function () {
         requestAnimationFrame( render );
@@ -130,14 +142,14 @@ crunches = [
 
 videoElement.addEventListener('loadedmetadata', (event) => {
     initRenderer();
+    stream = renderer.domElement.captureStream(); // frames per second
+    console.log('Started stream capture from canvas element: ', stream);
 })
 
 let isSoundPlayable = true;
 let wasPreviousMouthOpen = false;
 
 function onResults(results) {
-    // Hide the spinner.
-    document.body.classList.add('loaded');
 
     // get lip points
     if (results.multiFaceLandmarks) {
@@ -150,22 +162,27 @@ function onResults(results) {
                 Math.pow(landmarks[291].y - landmarks[62].y, 2);
 
             if (isMouthOpen) {
-                // canvasCtx.fillStyle = "#30ff30";
-                let target = screenSpaceToWorldSpace(
-                    (landmarks[13].x + landmarks[14].x)/2,
-                    (landmarks[13].y + landmarks[14].y)/2,
-                    0.2
-                );
-                const alpha = 0.3;
-                //console.log(target, cube.position);
-                edible.position.addVectors(edible.position.multiplyScalar(1 - alpha), target.multiplyScalar(alpha));
+                if (isFoodAvailable) {
+                    // canvasCtx.fillStyle = "#30ff30";
+                    let target = screenSpaceToWorldSpace(
+                        (landmarks[13].x + landmarks[14].x)/2,
+                        (landmarks[13].y + landmarks[14].y)/2,
+                        0.2
+                    );
+                    const alpha = 0.3;
+                    //console.log(target, cube.position);
+                    edible.position.addVectors(edible.position.multiplyScalar(1 - alpha), target.multiplyScalar(alpha));
+                }
 
             } else {
                 //canvasCtx.fillStyle = "#ff3030";
                 if (isSoundPlayable && wasPreviousMouthOpen) {
+                    next_button.disabled = false;
                     isSoundPlayable = false;
-                    crunches[getRandomInt(crunches.length)].play();
-                    edible.copy(edibles[getRandomInt(edibles.length)].clone());
+                    if (isFoodAvailable) {
+                        crunches[getRandomInt(crunches.length)].play();
+                        edible.copy(edibles[getRandomInt(edibles.length)].clone());
+                    }
                     setTimeout(
                         function() {isSoundPlayable = true;},
                         300
@@ -191,3 +208,94 @@ const mediapipeCamera = new Camera(videoElement, {
     height: 720
 });
 mediapipeCamera.start();
+
+const mediaSource = new MediaSource();
+mediaSource.addEventListener('sourceopen', handleSourceOpen, false);
+
+const recordButton = document.querySelector('button#record');
+const downloadButton = document.querySelector('button#download');
+recordButton.onclick = toggleRecording;
+downloadButton.onclick = download;
+
+function handleSourceOpen(event) {
+    console.log('MediaSource opened');
+    sourceBuffer = mediaSource.addSourceBuffer('video/webm; codecs="vp8"');
+    console.log('Source buffer: ', sourceBuffer);
+}
+
+function handleDataAvailable(event) {
+    if (event.data && event.data.size > 0) {
+        recordedBlobs.push(event.data);
+    }
+}
+
+function handleStop(event) {
+    console.log('Recorder stopped: ', event);
+    const superBuffer = new Blob(recordedBlobs, {type: 'video/webm'});
+    // video.src = window.URL.createObjectURL(superBuffer);
+}
+
+function toggleRecording() {
+    if (recordButton.textContent === 'Start Recording') {
+        startRecording();
+    } else {
+        stopRecording();
+        recordButton.textContent = 'Start Recording';
+        downloadButton.disabled = false;
+    }
+}
+
+// The nested try blocks will be simplified when Chrome 47 moves to Stable
+function startRecording() {
+    let options = {mimeType: 'video/webm'};
+    recordedBlobs = [];
+    try {
+        mediaRecorder = new MediaRecorder(stream, options);
+    } catch (e0) {
+        console.log('Unable to create MediaRecorder with options Object: ', e0);
+        try {
+            options = {mimeType: 'video/webm,codecs=vp9'};
+            mediaRecorder = new MediaRecorder(stream, options);
+        } catch (e1) {
+            console.log('Unable to create MediaRecorder with options Object: ', e1);
+            try {
+                options = 'video/vp8'; // Chrome 47
+                mediaRecorder = new MediaRecorder(stream, options);
+            } catch (e2) {
+                alert('MediaRecorder is not supported by this browser.\n\n' +
+                    'Try Firefox 29 or later, or Chrome 47 or later, ' +
+                    'with Enable experimental Web Platform features enabled from chrome://flags.');
+                console.error('Exception while creating MediaRecorder:', e2);
+                return;
+            }
+        }
+    }
+    console.log('Created MediaRecorder', mediaRecorder, 'with options', options);
+    recordButton.textContent = 'Stop Recording';
+    downloadButton.disabled = true;
+    mediaRecorder.onstop = handleStop;
+    mediaRecorder.ondataavailable = handleDataAvailable;
+    mediaRecorder.start(100); // collect 100ms of data
+    console.log('MediaRecorder started', mediaRecorder);
+}
+
+function stopRecording() {
+    mediaRecorder.stop();
+    console.log('Recorded Blobs: ', recordedBlobs);
+    // video.controls = true;
+}
+
+function download() {
+    const blob = new Blob(recordedBlobs, {type: 'video/webm'});
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = 'test.webm';
+    document.body.appendChild(a);
+    a.click();
+    setTimeout(() => {
+        document.body.removeChild(a);
+        window.URL.revokeObjectURL(url);
+    }, 100);
+}
